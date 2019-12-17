c92fa525d40dff0cb507eeb61a785a05985cd004
hanabira80 <hanabira80@gmail.com>
2019-12-17 13:35:10 +0900

    as

--- js/common.js
+++ js/common.js
@@ -13,8 +13,8 @@ function parallaxScroll(){
         $("header .scroll_nav .percent").css("transform","scale3d("+rate+",1,1)");
     }
     if (0 <= scrolled && winHeight >= scrolled) {
-        $(".subpage_visual").css("background-position","50% "+ scrolled/7*-1 +"px");
-        $(".subpage_visual .backdrop").css("background","rgba(0,0,0,"+ scrolled/winHeight*0.8 +")")
+        $(".full_page").css("background-position","50% "+ scrolled/7*-1 +"px");
+        $(".backdrop").css("background","rgba(0,0,0,"+ scrolled/winHeight*0.8 +")")
     }
     if (totalHeight - winHeight - 100 <= scrolled) {
         $("footer .footer-logo").addClass("js-is-active");
---
+++ js/sub.js
@@ -0,0 +1,18 @@
+function createCircle() {
+    const 
+    setEl = document.querySelector(".subpage_visual"),
+    setElHeight = setEl.offsetHeight,
+    setElWidth = setEl.offsetWidth,
+    circleEl = document.createElement('div');
+
+	circleEl.classList.add('circle')
+	circleEl.style.top = Math.random() * setElHeight + "px";
+	circleEl.style.left = Math.random() * setElWidth + "px";
+	setEl.appendChild(circleEl);
+	
+	setTimeout(() => {
+        circleEl.remove()
+    }, 3000)
+}
+
+setInterval(createCircle, 300);
